79c3b4254b9046e833c5b4b6731bc943b1b38e6d
Samuel Duflos <duflos.samuel@gmail.com>
2024-09-27 16:52:20 +0200

    Test

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/CVVueJS-Project/',
+  base: '/CVVueJS/dist',
   build: {
     outDir: 'dist'
   }
