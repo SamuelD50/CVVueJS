726f7b72d7c7bf28a6a5f853cdaa567395daeec6
Samuel Duflos <duflos.samuel@gmail.com>
2024-09-27 15:17:46 +0200

    Test

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
   import Education from './components/Education.vue';
   import Skills from './components/Skills.vue';
   import Interests from './components/Interests.vue';
-
   import Contact from './components/Contact.vue';
 </script>
 
@@ -32,63 +31,3 @@
     </div>
   </main>
 </template>
-
-<!-- <template>
-  <header class="bg-blue-500">
-    <Header/>
-  </header>
-
-  <main class="flex flex-row bg-gray-300">
-    <div>
-      <Profile/>
-      <WorkExperiences/>
-    </div>
-    <div>
-      <Education/>
-      <Skills/>
-    </div>
-    <div>
-      <Interests/>
-    </div>
-    
-  </main>
-
-  <footer class="bg-green-300">
-    <Contact/>
-
-  </footer>
-</template> -->
-
-
-<!-- <style scoped>
-header {
-  background-color: blue;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-footer {
-  background-color: green;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style> -->
--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/CVVueJS/index.html'
+  base: '/CVVueJS/',
+  build: {
+    outDir: 'dist'
+  }
 })
